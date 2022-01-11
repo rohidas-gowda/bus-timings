bb89dc85d75be2ebb33ef3f1f978864cbcb4057d
Rohidas Gowda <rohidasgowda@gmail.com>
2022-01-11 19:41:32 +0530

    feat: screen sweeping between search bus, reserved services and station info.

--- app/src/main/java/com/andromojo/bustimings/MainActivity.java
+++ app/src/main/java/com/andromojo/bustimings/MainActivity.java
@@ -1,14 +1,30 @@
 package com.andromojo.bustimings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayoutAdapter tabLayoutAdapter;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    private String[] tabItemNames = new String[]{"Search Bus", "Reserved", "Stations"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayoutAdapter = new TabLayoutAdapter(this);
+        viewPager2.setAdapter(tabLayoutAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, ((tab, position) -> tab.setText(tabItemNames[position]))).attach();
     }
 }
---
+++ app/src/main/java/com/andromojo/bustimings/TabLayoutAdapter.java
@@ -0,0 +1,30 @@
+package com.andromojo.bustimings;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabLayoutAdapter extends FragmentStateAdapter {
+    private String[] tabItemNames = new String[]{"Search Bus", "Reserved", "Stations"};
+
+    public TabLayoutAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0: return new SearchBusInfo();
+            case 1: return new ReservedServicesInfo();
+            case 2: return new BusStationInfo();
+        }
+        return new SearchBusInfo();
+    }
+
+    @Override
+    public int getItemCount() {
+        return tabItemNames.length;
+    }
+}
